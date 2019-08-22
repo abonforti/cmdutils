4357acd7a1dba35819a3f3b2a2da22289172fe10
Alessio Bonforti <a.bonforti@accenture.com>
2019-08-22 13:40:44 +0200

    Updating some files

--- listpods.sh
+++ listpods.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ $1 = "dev" ]; then
+		kubectl config use-context goep-dev-aksap
     watch -n1 "kubectl -n dev-ap1 get pods | grep ^hybris-"
 elif [ $1 = "syt" ]; then
+		kubectl config use-context goep-tst-aksap
     watch -n1 "kubectl -n syt-ap1 get pods | grep ^hybris-"
 elif [ $1 = "int" ]; then
     printf "Not yet implemented"
